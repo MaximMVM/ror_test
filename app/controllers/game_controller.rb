344cbe4275a69b0596e9f675ffbee3f597676acf
MaximMVM <maksim1981m@gmail.com>
2022-07-11 07:44:20 +0300

    resolve a few bugs, add style

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,34 +1,42 @@
 class GameController < ApplicationController
+  @@result = nil # for change data between variable because every HTTP-request create new instanse GameController
+  @@cpu = nil
+
   def index
-    units = %w(камень ножницы бумага)
-    @cpu_input = units[rand(0..2)]
-    @user_input = params[:unit]
-    @result = calc(@user_input, @cpu_input)
+    @result = @@result 
+    @cpu_input = @@cpu
   end
+
+  def calc
+    units = %w(камень ножницы бумага)
+    serv = units[rand(0..2)]
+    @@cpu = serv
+    user = params[:unit]
+
+    # could rules class variable but it smal and I'm lazy
+    rules = {"камень"=> "ножницы",
+             "ножницы" => "бумага",
+             "бумага"=> "камень",
+            }
+   if serv == user
+      result = "Ничья, вы оба загадали #{user}"
+   else
+   
+     rules.each do |key, value|
+       if serv == key and user == value
+          result = "Сервер победил! Он загадал #{serv}, а вы #{user}"
+          break
+       elsif user == key and serv == value
+           result = "Вы победили! Вы загадали #{user}, а сервер #{serv}"
+           break
+       end
+     end
+    end
+   @@result = result
+   redirect_back(fallback_location: gi_path)
 
 end
 
-def calc(user, serv)
+end
 
-      # @user_input = params[:unit]
-      result = nil
-      rules = {"камень"=> "ножницы",
-               "ножницы" => "бумага",
-               "бумага"=> "камень",
-              }
-     if serv == user
-        result = "Ничья, вы оба загадали #{user}"
-     end
-     
-     rules.each do |key, value|
-		 if serv == key and user == value
-			 result = "Сервер победил, он загадал #{serv}, а вы #{user}"
-		     break
-		 elsif user == key and serv == value
-		     result = "Вы победили! Вы загадали #{user}, а сервер #{serv}"
-		     break
-		 end
-     end
-     # redirect_back(fallback_location: gi_path)
-    result  
- end
+
